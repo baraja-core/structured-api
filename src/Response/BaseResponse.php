9529f97b56cb2237980844e77292b741d5d5099d
Jan Barášek <janbarasek@gmail.com>
2020-01-06 13:14:21 +0100

    Response: Format response objects and data automatically + remove dangerous keys like password

--- src/Response/BaseResponse.php
+++ src/Response/BaseResponse.php
@@ -5,8 +5,15 @@ declare(strict_types=1);
 namespace Baraja\StructuredApi;
 
 
+use Tracy\Debugger;
+use Tracy\ILogger;
+
 abstract class BaseResponse
 {
+
+	public static $keysToHide = ['password', 'passwd', 'pass', 'pwd', 'creditcard', 'credit card', 'cc', 'pin'];
+
+	public static $hiddenKeyLabel = '*****';
 
 	/**
 	 * @var mixed[]
@@ -35,6 +42,42 @@ abstract class BaseResponse
 	public function __toString(): string
 	{
 		return '';
+	}
+
+	/**
+	 * @param mixed $key
+	 * @param mixed $value
+	 * @return bool
+	 */
+	protected function hideKey($key, $value): bool
+	{
+		static $hide;
+
+		if ($hide === null) {
+			$hide = [];
+			foreach (self::$keysToHide as $hideKey) {
+				$hide[$hideKey] = true;
+			}
+		}
+
+		if (isset($hide[$key]) === true) {
+			if (preg_match('/^\$2[ayb]\$.{56}$/', $value)) { // Allow BCrypt hash only.
+				return false;
+			}
+
+			if (\class_exists(Debugger::class) === true) {
+				Debugger::log(
+					new RuntimeStructuredApiException(
+						'Security warning: User password may have been compromised! Key "' . $key . '" given.'
+						. "\n" . 'The Baraja API prevented passwords being passed through the API in a readable form.'
+					), ILogger::CRITICAL
+				);
+			}
+
+			return true;
+		}
+
+		return false;
 	}
 
 }

021b8e5bca7b2c3d5ef8ff7b032a2da58c6e11a3
Jan Barášek <janbarasek@gmail.com>
2020-02-12 12:27:22 +0100

    JsonResponse: Code simplify.

--- src/Response/JsonResponse.php
+++ src/Response/JsonResponse.php
@@ -37,55 +37,7 @@ final class JsonResponse extends BaseResponse
 	 */
 	public function getJson(): string
 	{
-		return Json::encode($this->process($this->haystack), Json::PRETTY);
-	}
-
-	/**
-	 * Convert common haystack to json compatible format.
-	 *
-	 * @param mixed $haystack
-	 * @return array|string|mixed
-	 */
-	private function process($haystack)
-	{
-		if (\is_array($haystack) === true) {
-			$return = [];
-
-			foreach ($haystack as $key => $value) {
-				$return[$key] = $this->hideKey($key, $value) ? self::$hiddenKeyLabel : $this->process($value);
-			}
-
-			return $return;
-		}
-
-		if (\is_object($haystack) === true) {
-			if (\method_exists($haystack, '__toString') === true) {
-				return (string) $haystack;
-			}
-
-			$return = [];
-
-			try {
-				foreach ((new \ReflectionClass($haystack))->getProperties() as $property) {
-					$property->setAccessible(true);
-
-					if (($key = $property->getName()) && ($key[0] ?? '') === '_') {
-						continue;
-					}
-
-					$value = $property->getValue($haystack);
-					$return[$key] = $this->hideKey($key, $value) ? self::$hiddenKeyLabel : $this->process($value);
-				}
-			} catch (\ReflectionException $e) {
-				foreach ($haystack as $key => $value) {
-					$return[$key] = $this->hideKey($key, $value) ? self::$hiddenKeyLabel : $this->process($value);
-				}
-			}
-
-			return $return;
-		}
-
-		return $haystack;
+		return Json::encode($this->toArray(), Json::PRETTY);
 	}
 
 }

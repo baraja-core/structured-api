b820db53533d7ed403903b3f3fab54c32f5b0675
Jan Barášek <janbarasek@gmail.com>
2021-01-20 10:05:38 +0100

    Use safe baraja-core/url.

--- src/Helpers.php
+++ src/Helpers.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Baraja\StructuredApi;
 
 
+use Baraja\Url\Url;
 use Nette\Http\Request;
 use Nette\Utils\Strings;
 
@@ -24,44 +25,7 @@ final class Helpers
 	 */
 	public static function processPath(Request $httpRequest): string
 	{
-		return trim(str_replace(rtrim($httpRequest->getUrl()->withoutUserInfo()->getBaseUrl(), '/'), '', (string) self::getCurrentUrl()), '/');
-	}
-
-
-	/**
-	 * Return current absolute URL.
-	 * Return null, if current URL does not exist (for example in CLI mode).
-	 */
-	public static function getCurrentUrl(): ?string
-	{
-		if (!isset($_SERVER['REQUEST_URI'], $_SERVER['HTTP_HOST'])) {
-			return null;
-		}
-
-		return (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https' : 'http')
-			. '://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-	}
-
-
-	public static function getBaseUrl(): ?string
-	{
-		static $return;
-
-		if ($return !== null) {
-			return $return;
-		}
-		if (($currentUrl = self::getCurrentUrl()) !== null) {
-			if (preg_match('/^(https?:\/\/.+)\/www\//', $currentUrl, $localUrlParser)) {
-				$return = $localUrlParser[0];
-			} elseif (preg_match('/^(https?:\/\/[^\/]+)/', $currentUrl, $publicUrlParser)) {
-				$return = $publicUrlParser[1];
-			}
-		}
-		if ($return !== null) {
-			$return = rtrim($return, '/');
-		}
-
-		return $return;
+		return trim(str_replace(rtrim($httpRequest->getUrl()->withoutUserInfo()->getBaseUrl(), '/'), '', Url::get()->getCurrentUrl()), '/');
 	}
 
 

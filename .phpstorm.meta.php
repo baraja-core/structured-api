16152dc448557f6694e59988dfe6b6d7b73f66cb
Jan Barášek <jan.barasek@packeta.com>
2021-05-19 15:10:50 +0200

    PhpStorm meta: Define flashMessage arguments.

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -11,3 +11,12 @@ exitPoint(\Baraja\StructuredApi\BaseEndpoint::sendError());
 exitPoint(\Baraja\StructuredApi\BaseEndpoint::sendItems());
 exitPoint(\Baraja\StructuredApi\BaseEndpoint::redirect());
 exitPoint(\Baraja\StructuredApi\BaseEndpoint::redirectUrl());
+
+expectedArguments(
+	\Baraja\StructuredApi\BaseEndpoint::flashMessage(),
+	1,
+	\Baraja\StructuredApi\BaseEndpoint::FLASH_MESSAGE_SUCCESS,
+	\Baraja\StructuredApi\BaseEndpoint::FLASH_MESSAGE_INFO,
+	\Baraja\StructuredApi\BaseEndpoint::FLASH_MESSAGE_WARNING,
+	\Baraja\StructuredApi\BaseEndpoint::FLASH_MESSAGE_ERROR,
+);

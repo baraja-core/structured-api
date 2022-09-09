fab2f1c6e8bd7a9260a342f74279897a78b1aa98
Jan Barášek <janbarasek@gmail.com>
2022-09-09 13:04:26 +0200

    JsonResponse: Remove dependency to Nette/Utils/Json.

--- src/Response/JsonResponse.php
+++ src/Response/JsonResponse.php
@@ -5,8 +5,6 @@ declare(strict_types=1);
 namespace Baraja\StructuredApi;
 
 
-use Nette\Utils\Json;
-use Nette\Utils\JsonException;
 use Tracy\Debugger;
 use Tracy\ILogger;
 
@@ -27,14 +25,21 @@ class JsonResponse extends BaseResponse
 	public function getJson(): string
 	{
 		try {
-			return Json::encode($this->toArray(), Json::PRETTY);
-		} catch (JsonException $e) {
+			return json_encode(
+				$this->toArray(),
+				JSON_THROW_ON_ERROR
+				| JSON_UNESCAPED_SLASHES
+				| JSON_UNESCAPED_UNICODE
+				| JSON_PRETTY_PRINT
+				| (defined('JSON_PRESERVE_ZERO_FRACTION') ? JSON_PRESERVE_ZERO_FRACTION : 0),
+			);
+		} catch (\JsonException $e) {
 			trigger_error($e->getMessage());
 			if (class_exists('\Tracy\Debugger') === true) {
 				Debugger::log($e, ILogger::CRITICAL);
 			}
-
-			return '{}';
 		}
+
+		return '{}';
 	}
 }
